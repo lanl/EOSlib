2dbba4999e027556f15acfa96b179f145d19af0e
Nathan Woods <woodscn@lanl.gov>
2016-06-24 08:55:23 -0600

    Determine shared object/dynamic library file extension at runtime.

--- src/DataBase/Subs/Handle.C
+++ src/DataBase/Subs/Handle.C
@@ -1,6 +1,7 @@
 #include <dlfcn.h>
 #include <cstdlib>
 #include "Handle.h"
+#include <sstream>
 
 Handle::Handle() : handle(NULL), del(0)
 {
@@ -19,9 +20,21 @@ void Handle::copy(const Handle &h)
     del = 0;
 }
 
+#ifdef __APPLE__
+#define SHARED_LIB_EXT ".dylib"
+#elif __unix__
+#define SHARED_LIB_EXT ".so"
+#elif _WIN32
+#define SHARED_LIB_EXT ".dll"
+#endif
+
 int Handle::dlopen(const char *shared_obj)
 {
-    handle = ::dlopen(shared_obj, RTLD_NOW | RTLD_GLOBAL);
+    std::ostringstream oss;
+    oss << shared_obj << SHARED_LIB_EXT;
+    //    handle = ::dlopen(shared_obj, RTLD_NOW | RTLD_GLOBAL);
+    auto s = oss.str();
+    handle = ::dlopen(s.c_str(), RTLD_NOW | RTLD_GLOBAL);
     if( handle )
     {
         del = 1;
@@ -30,6 +43,10 @@ int Handle::dlopen(const char *shared_obj)
     else
         return 1;
 }
+
+#ifdef SHARED_LIB_EXT
+#undef SHARED_LIB_EXT
+#endif
 
 const char *Handle::dlerror()
 {
