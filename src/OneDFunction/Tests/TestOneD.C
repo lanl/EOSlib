e24d4ec6fca97b700bfbeefe8fbef52b9c8cfd51
Ralph Menikoff <rsm@cjdet2.lanl.gov>
2017-01-22 12:40:13 -0700

    complete restructure of CMakeLists.txt rename subdir Eos to EOS with corresponding changes to DATA and blessed_output update from previous changes to old EOSlib

---
+++ src/OneDFunction/Tests/TestOneD.C
@@ -0,0 +1,42 @@
+#include <Arg.h>
+#include <iostream>
+
+#include "OneDFunction.h"
+
+using namespace std;
+class Funct : public OneDFunction
+{
+public:
+	double f(double x) { return atan(x);}
+	
+};
+
+int main(int, char **argv)
+{
+	Funct A;
+	
+	double x0 = -5;
+	double x1 = 5;
+	double guess=HUGE;
+	double y_goal = 0.0;
+	
+	while(*++argv)
+	{
+		GetVar2(bracket, x0, x1);
+		GetVar(guess, guess);
+		GetVar(y,y_goal);
+		ArgError;
+	}
+	
+	double x = A.inverse(y_goal, x0,x1,guess);
+	
+	cout << "Status: " << A.ErrorStatus()
+		<< "  x, y = " << x << " " << y_goal << "\n";
+	
+	cout << "Iterations = " << A.Iterations() << "\n";
+	
+	cout << A.x0 << " " << A.y0 << " 	 "
+		<< A.x1 << " " << A.y1 << "\n";
+	
+	return 0;
+}
