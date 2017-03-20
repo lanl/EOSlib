b96e72cca990090d40491866a3446844b654d4b7
Nathan Woods <woodscn@lanl.gov>
2017-03-20 16:08:19 -0600

    Add stiffened gas example to LowLevelHugoniot.C, not working yet.

--- Examples/LowLevelHugoniot.C
+++ Examples/LowLevelHugoniot.C
@@ -1,13 +1,13 @@
 #include <EOS.h>
 #include <IdealGas.h>
 
-int main()
-{
-    // Define EOS
-    EOS *eos = new IdealGas(1.4);   // gamma = 1.4
+#include <StiffenedGas.h>
 
+int common(EOS *eos)
+{
     // Define initial hydro state
     HydroState state(1.0,1.0,0.0);  // V1 = 1, e1 = 1, u1 = 0
+
     // Print initial state
     std::cout << "Ahead state\n";
     HydroStateLabel(std::cout) << " " << Center(P_form, "P")
@@ -16,6 +16,7 @@ int main()
     std::cout << state << " " << P_form << eos->P(state)
                        << " " << c_form << eos->c(state)
                        << "\n";
+    
     // Initialize Hugoniot
     Hugoniot *hugoniot = eos->shock(state);
     
@@ -29,6 +30,7 @@ int main()
         std::cout << "Error, hugoniot failed\n";
         return 1;
     }
+
     // Print shock state
     std::cout << "Shock wave state\n";
     WaveStateLabel(std::cout) << "\n";    
@@ -40,3 +42,55 @@ int main()
 
     return 0;
 }
+
+EOS* ideal_gas()
+{
+    // Define EOS
+    // Ideal gas EOS can be called directly, unlike most others.
+    EOS *eos = new IdealGas(1.4);   // gamma = 1.4
+    return eos;
+}
+
+EOS* stiffened_gas()
+{
+    // Define EOS
+    // 
+    // Most EOS derived classes have too many parameters to be effectively set
+    // as arguments to the constructor. If you really want to set these 
+    // directly, instead of through the database, then you can do it like this:
+    Parameters param; 
+    param.base = Strdup("EOS");
+    param.type = Strdup("StiffenedGas");
+    param.name = Strdup("HMX");
+    param.Append("c0","2.6778");
+    param.Append("s","2.68946");
+    param.Append("P0=0.0001");
+    param.Append("Cv=1.05e-3");
+    param.Append("e_ref=0.0");
+    param.Append("V_ref=0.52");
+
+    std::cout << "Got this far\n";
+    std::cout.flush();
+
+    StiffenedGas *eos;
+    if( eos->EOS::Initialize(param) ) 
+    {
+	    std::cout << "Error, Initialize failed\n"; 
+    }
+    std::cout << "Got this far\n";
+    std::cout.flush();
+    return eos;
+}
+    
+int main()
+{ 
+    std::cout << "Ideal Gas: \n";
+    EOS* eos = ideal_gas();
+    common(eos);
+    std::cout << "Stiffened Gas: \n";
+    EOS* eos2 = stiffened_gas();
+    common(eos);
+
+}
+
+
